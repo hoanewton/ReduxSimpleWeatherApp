8d5b73b93b642e9077b4a2ea0001071adadce842
Hoa Newton <hoa@ellevatenetwork.com>
2018-09-13 08:38:01 -0400

    add middleware, action creator for fetching weather

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'a4f997393790f0b5d92dc78cdfdfadaf';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forcast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather (city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
